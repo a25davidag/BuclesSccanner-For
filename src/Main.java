a830d457f3e1e3c2abf4c3526f80f87a45147eb7
David Arranz González <a25davidag@teis.intranet>
2025-09-16 16:46:57 +0200

    bucles en for

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Ejercicio 1
+        // Ejercicio 1 (for)
         System.out.print("Ingresa un numero");
         //sc.nextLine();
-        int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
+        int n1 = sc.nextInt();
+        for (int i = 1; i <= n1; i++) {
             System.out.println(i);
-
-
         }
+
+        // Ejercicio 2
+        System.out.println("Ingresa un numero");
+        //sc.nextLine();
+        int n2 = sc.nextInt();
+        for (int e = n2; e >= 1; e--) {
+            System.out.println(e);
+        }
+
+        // Ejercicio 3
+        System.out.println("Ingresa numero");
+        //sc.nextLine();
+        int n3 = sc.nextInt();
+        for (int f = 1; f <= 10; f++) {
+            System.out.println(n3 + "x" + f + "=" + (n3 * f));
+        }
+
+        // Ejercioio 4
+        System.out.println("Ingresa numero");
+        sc.nextLine();
+        int n4 = sc.nextInt();
+        int suma = 0;
+        for (int h = 1; h <= n4; h++) {
+            suma = suma + h;
+        }
+        System.out.println("La suma " + n4 + " es: " + suma);
     }
-    }
+}
