d55904f7452ff534eb0117fc3a3b693da8a54d33
David Arranz González <a25davidag@teis.intranet>
2025-09-16 15:42:12 +0200

    scanner2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Ejercicio 1
+        System.out.print("Ingresa un numero");
+        //sc.nextLine();
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+
 
         }
+    }
     }
