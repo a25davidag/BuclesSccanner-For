e4518e48c7bf7669071ff6360d0835f2c206d35e
dabid chu <arranzd5@gmail.com>
2025-10-05 13:00:06 +0200

    enunciados

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Ejercicio 1 (for)
+        // Ejercicio 1 (for) Contador simple (for): Ingresa un número N y muestra los números del 1 al N
         System.out.print("Ingresa un numero");
         //sc.nextLine();
         int n1 = sc.nextInt();
@@ -14,7 +14,7 @@ public class Main {
             System.out.println(i);
         }
 
-        // Ejercicio 2
+        // Ejercicio 2 Cuenta regresiva (for): Ingresa un número N y muestra una cuenta atrás desde N hasta 1.
         System.out.println("Ingresa un numero");
         //sc.nextLine();
         int n2 = sc.nextInt();
@@ -22,7 +22,7 @@ public class Main {
             System.out.println(e);
         }
 
-        // Ejercicio 3
+        // Ejercicio 3  Tabla de multiplicar (for): Ingresa un número y muestra su tabla de multiplicar del 1 al 10.
         System.out.println("Ingresa numero");
         //sc.nextLine();
         int n3 = sc.nextInt();
@@ -30,7 +30,7 @@ public class Main {
             System.out.println(n3 + "x" + f + "=" + (n3 * f));
         }
 
-        // Ejercioio 4
+        // Ejercioio 4 Sumatoria (for): Ingresa un número N y calcula la suma de los números del 1 al N.
         System.out.println("Ingresa numero");
         sc.nextLine();
         int n4 = sc.nextInt();
@@ -40,7 +40,7 @@ public class Main {
         }
         System.out.println("La suma " + n4 + " es: " + suma);
 
-        // Ejercicio 10
+        // Ejercicio 10  Números impares (for): Ingresa un número N y muestra los impares desde 1 hasta N.
         System.out.println("Ingresa un numero");
         int n5 = sc.nextInt();
         for (int j = 1; j <= n5; j++) {
